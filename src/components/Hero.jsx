12e1612b6da333d8691ba6c71a0f3cfc2487dc2f
atanu <atanu.tmsl@gmail.com>
2023-08-12 12:12:48 +0530

    feat: responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,48 +35,48 @@ const Hero = () => {
       {/* text-section */}
 
       <div className="hero-box-txt">
-      <div className="main-heading">COMMUNITY DRIVEN </div>
-      <div className="sub-heading">TOOLS FOR EVERYONE</div> 
+        <div className="main-heading">COMMUNITY DRIVEN </div>
+        <div className="sub-heading">TOOLS FOR EVERYONE</div>
 
         <div className="main-box">
-        <div className="box">
-        <div className="heading">Trade Options</div>
-        <div className="sub">
-          Trade capital efficient option spreads. Traders just need to post the
-          max loss possible and Otus handles the rest.
-        </div>
-        </div>
-        <div className="box">
-        <div className="heading">Provide Liquidity</div>
-        <div className="sub">
-          Earn fees from option spread traders, no withdrawal fees, capital is
-          automatically hedged.
-        </div>
-        </div>
-        <div className="box">
-        <div className="heading">Ranged Markets</div>
-        <div className="sub">
-          Easily bet on the market direction through IN or OUT tokens.
-        </div>
-        </div>
-        <div className="box">
-        <div className="heading">Earn Vaults</div>
-        <div className="sub">
-          Create your own unique and private or public options vault with
-          hedging tools.
-        </div>
-        </div>
-        <div className="box">
-        <div className="heading">Trade One Click Option Strategies</div>
-        <div className="sub">
-          Execute multi leg trades on Lyra and other platforms in 1 click with
-          Otus.
-        </div>
-        </div>
-        <div className="box">
-        <div className="heading">One Click Hedge</div>
-        <div className="sub">Hedge your Lyra position with one click.</div>
-        </div>
+          <div className="box">
+            <div className="heading">Trade Options</div>
+            <div className="sub">
+              Trade capital efficient option spreads. Traders just need to post
+              the max loss possible and Otus handles the rest.
+            </div>
+          </div>
+          <div className="box">
+            <div className="heading">Provide Liquidity</div>
+            <div className="sub">
+              Earn fees from option spread traders, no withdrawal fees, capital
+              is automatically hedged.
+            </div>
+          </div>
+          <div className="box">
+            <div className="heading">Ranged Markets</div>
+            <div className="sub">
+              Easily bet on the market direction through IN or OUT tokens.
+            </div>
+          </div>
+          <div className="box">
+            <div className="heading">Earn Vaults</div>
+            <div className="sub">
+              Create your own unique and private or public options vault with
+              hedging tools.
+            </div>
+          </div>
+          <div className="box">
+            <div className="heading">Trade One Click Option Strategies</div>
+            <div className="sub">
+              Execute multi leg trades on Lyra and other platforms in 1 click
+              with Otus.
+            </div>
+          </div>
+          <div className="box">
+            <div className="heading">One Click Hedge</div>
+            <div className="sub">Hedge your Lyra position with one click.</div>
+          </div>
         </div>
       </div>
     </div>
